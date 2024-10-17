9ab1e93f297a96f4dee895db0192c0f44b8ffcbc
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-10-17 16:12:49 +0300

    feat(utils): add format number util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
+
+function formatNumber(num: number) {
+  const thresholds: Record<string, string> = {
+    1000000: "M",
+    1000: "K",
+  };
+
+  const threshold = Object.keys(thresholds)
+    .sort((a, b) => Number(b) - Number(a))
+    .find((threshold) => num >= Number(threshold));
+
+  return threshold
+    ? Math.floor(num / Number(threshold)).toString() + thresholds[threshold]
+    : num.toString();
+}
+
+export { cn, formatNumber };
