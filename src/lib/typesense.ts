91ef1bd9fd5cefc961373fa56499789713d99a66
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-10-17 16:20:54 +0300

    feat: add typesense config utils

---
+++ src/lib/typesense.ts
@@ -0,0 +1,20 @@
+import type { ConfigurationOptions } from "typesense/lib/Typesense/Configuration";
+
+const {
+  TYPESENSE_HOST = "localhost",
+  TYPESENSE_PORT = 8108,
+  TYPESENSE_PROTOCOL = "http",
+  TYPESENSE_API_KEY = "xyz",
+} = process.env;
+
+export const typesenseConfig: ConfigurationOptions = {
+  nodes: [
+    {
+      host: TYPESENSE_HOST,
+      port: Number(TYPESENSE_PORT),
+      protocol: TYPESENSE_PROTOCOL,
+    },
+  ],
+  apiKey: TYPESENSE_API_KEY,
+  retryIntervalSeconds: 2,
+};
