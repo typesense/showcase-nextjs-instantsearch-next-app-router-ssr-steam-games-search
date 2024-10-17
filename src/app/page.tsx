526c6755e78002849332e54b51ae250b3108f3c9
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-10-17 16:40:20 +0300

    feat: add background gradient to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 container mx-auto items-start gap-10 py-8 px-5 md:px-0"></main>
+      <div className="fixed inset-0 z-[-2] bg-background bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(161,161,170,0.2),rgba(255,255,255,0))]"></div>
+      <main className="flex-1 container mx-auto items-start gap-10 py-8 md:px-0">
+      </main>
     </div>
   );
 }
